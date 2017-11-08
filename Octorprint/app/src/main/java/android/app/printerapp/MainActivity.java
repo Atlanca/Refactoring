3f85b0b59089fc5e4b0a66a5584389c9c0e4dc6f
Alex Tao <talex@student.chalmers.se>
2017-11-08 12:12:03 +0100

    first

---
+++ Octorprint/app/src/main/java/android/app/printerapp/MainActivity.java
@@ -0,0 +1,262 @@
+package android.app.printerapp;
+
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.app.printerapp.devices.database.DatabaseController;
+import android.app.printerapp.library.LibraryController;
+import android.app.printerapp.library.LibraryFragment;
+import android.app.printerapp.library.detail.DetailViewFragment;
+import android.app.printerapp.util.ui.AnimationHelper;
+import android.app.printerapp.viewer.ViewerMainFragment;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.res.Configuration;
+import android.database.Cursor;
+import android.graphics.drawable.ColorDrawable;
+import android.net.wifi.WifiManager;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.StrictMode;
+import android.support.v4.content.LocalBroadcastManager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TabHost;
+import android.widget.TextView;
+import android.widget.Toast;
+
+/**
+ * Created by alberto-baeza on 1/21/15.
+ */
+public class MainActivity extends ActionBarActivity {
+
+    //List of Fragments
+    private LibraryFragment mLibraryFragment; //Storage fragment
+    private ViewerMainFragment mViewerFragment; //Print panel fragment @static for model load
+
+    //Class specific variables
+    private static Fragment mCurrent; //The current shown fragment @static
+    private static FragmentManager mManager; //Fragment manager to handle transitions @static
+    private static DialogController mDialog; //Dialog controller @static
+
+    private static TabHost mTabHost;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        /**
+         * Since API level 11, thread policy has changed and now does not allow network operation to
+         * be executed on UI thread (NetworkOnMainThreadException), so we have to add these lines to
+         * permit it.
+         */
+        if (android.os.Build.VERSION.SDK_INT > 9) {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+
+        mTabHost = (TabHost) findViewById(R.id.tabHost);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+        //Initialize variables
+        mManager = getFragmentManager();
+        mDialog = new DialogController(this);
+
+        //Initialize fragments
+        mViewerFragment = (ViewerMainFragment) getFragmentManager().findFragmentByTag(ListContent.ID_VIEWER);
+
+        mManager.addOnBackStackChangedListener(new FragmentManager.OnBackStackChangedListener() {
+            @Override
+            public void onBackStackChanged() {
+
+            }
+        });
+
+        //Set tab host for the view
+        setTabHost();
+
+    }
+
+    public static void performClick(int i){
+        mTabHost.setCurrentTab(i);
+    }
+
+    public void setTabHost() {
+
+        mTabHost.setup();
+
+        //Models tab
+        TabHost.TabSpec spec = mTabHost.newTabSpec("Library");
+        spec.setIndicator(getTabIndicator(getResources().getString(R.string.fragment_models)));
+        spec.setContent(R.id.maintab1);
+        mTabHost.addTab(spec);
+
+        //Print panel tab
+        spec = mTabHost.newTabSpec("Panel");
+        spec.setIndicator(getTabIndicator(getResources().getString(R.string.fragment_print)));
+        spec.setContent(R.id.maintab2);
+        mTabHost.addTab(spec);
+
+        //Print view tab
+        spec = mTabHost.newTabSpec("Printer");
+        spec.setIndicator(getTabIndicator(getResources().getString(R.string.fragment_devices)));
+        spec.setContent(R.id.maintab3);
+        //mTabHost.addTab(spec);
+
+        if (DatabaseController.count() > 0){
+            mTabHost.setCurrentTab(0);
+            onItemSelected(0);
+        } else {
+            mTabHost.setCurrentTab(1);
+            onItemSelected(1);
+
+        }
+
+
+        mTabHost.getTabWidget().setDividerDrawable(new ColorDrawable(getResources().getColor(R.color.transparent)));
+
+        mTabHost.setOnTabChangedListener(new TabHost.OnTabChangeListener() {
+            @Override
+            public void onTabChanged(String tabId) {
+
+                View currentView = mTabHost.getCurrentView();
+                AnimationHelper.inFromRightAnimation(currentView);
+
+                onItemSelected(mTabHost.getCurrentTab());
+
+            }
+        });
+
+    }
+
+    /**
+     * Return the custom view of the tab
+     *
+     * @param title Title of the tab
+     * @return Custom view of a tab layout
+     */
+    private View getTabIndicator(String title) {
+        View view = LayoutInflater.from(getApplicationContext()).inflate(R.layout.main_activity_tab_layout, null);
+        TextView tv = (TextView) view.findViewById(R.id.tab_title_textview);
+        tv.setText(title);
+        return view;
+    }
+
+    public void onItemSelected(int id) {
+
+        if (id!= 1) {
+
+            ViewerMainFragment.hideActionModePopUpWindow();
+            ViewerMainFragment.hideCurrentActionPopUpWindow();
+        }
+
+        Log.i("OUT", "Pressed " + id);
+        //start transaction
+        FragmentTransaction fragmentTransaction = mManager.beginTransaction();
+
+        //Pop backstack to avoid having bad references when coming from a Detail view
+        mManager.popBackStack();
+
+        //If there is a fragment being shown, hide it to show the new one
+        if (mCurrent != null) {
+            try {
+                fragmentTransaction.hide(mCurrent);
+            } catch (NullPointerException e) {
+                e.printStackTrace();
+            }
+        }
+
+        //Select fragment
+        switch (id) {
+
+            case 0: {
+                //Check if we already created the Fragment to avoid having multiple instances
+                if (getFragmentManager().findFragmentByTag(ListContent.ID_LIBRARY) == null) {
+                    mLibraryFragment = new LibraryFragment();
+                    fragmentTransaction.add(R.id.maintab1, mLibraryFragment, ListContent.ID_LIBRARY);
+                }
+                mCurrent = mLibraryFragment;
+
+            }
+
+            break;
+            case 1: {
+                closeDetailView();
+                //Check if we already created the Fragment to avoid having multiple instances
+                if (getFragmentManager().findFragmentByTag(ListContent.ID_VIEWER) == null) {
+                    mViewerFragment = new ViewerMainFragment();
+                    fragmentTransaction.add(R.id.maintab2, mViewerFragment, ListContent.ID_VIEWER);
+                }
+                mCurrent = mViewerFragment;
+            }
+            break;
+            case 2: {
+                break;
+            }
+        }
+            if (mViewerFragment != null) {
+                if (mCurrent != mViewerFragment) {
+                    //Make the surface invisible to avoid frame overlapping
+                    mViewerFragment.setSurfaceVisibility(0);
+
+                } else {
+                    //Make the surface visible when we press
+                    mViewerFragment.setSurfaceVisibility(1);
+                }
+            }
+
+            //Show current fragment
+            if (mCurrent != null) {
+                Log.i("OUT", "Changing " + mCurrent.getTag());
+                fragmentTransaction.show(mCurrent).commit();
+            }
+
+    }
+
+    public static void closeDetailView(){
+        //Refresh printview fragment if exists
+        Fragment fragment = mManager.findFragmentByTag(ListContent.ID_DETAIL);
+        if (fragment != null) ((DetailViewFragment) fragment).removeRightPanel();
+    }
+
+    //Show dialog
+    public static void showDialog(String msg) {
+        mDialog.displayDialog(msg);
+    }
+
+    /**
+     * Send a file to the Viewer to display
+     *
+     * @param path File path
+     */
+    public static void requestOpenFile(final String path) {
+
+        //This method will simulate a click and all its effects
+        performClick(1);
+
+        //Handler will avoid crash
+        Handler handler = new Handler();
+        handler.post(new Runnable() {
+
+            @Override
+            public void run() {
+
+                if (path!=null) ViewerMainFragment.openFileDialog(path);
+            }
+        });
+
+    }
+}
